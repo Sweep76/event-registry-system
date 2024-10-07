b2d6ff9c64a185c021fd2d8dc2e4a95284c8a3ae
Sweep <joshuapatrickchiu76@gmail.com>
2024-10-07 20:41:22 +0800

    fix: managed profile page

--- login/login.php
+++ login/login.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $dbConnection = getDatabaseConnection();
 
         $statement = $dbConnection->prepare(
-            "SELECT id, first_name, last_name, phone, password, created_at FROM users WHERE email = ?"
+            "SELECT id, first_name, last_name, phone, address, password, created_at FROM users WHERE email = ?"
         );
 
         // Bind variables to the prepared statement as parameters
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $statement->execute();
 
         // bind result variables
-        $statement->bind_result($id, $first_name, $last_name, $phone, $stored_password, $created_at);
+        $statement->bind_result($id, $first_name, $last_name, $phone, $address, $stored_password, $created_at);
 
         //fetch values
         if ($statement->fetch()) {
@@ -45,7 +45,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $_SESSION["last_name"] = $last_name;
                 $_SESSION["email"] = $email;
                 $_SESSION["phone"] = $phone;
+                $_SESSION["address"] = $address;
                 $_SESSION["created_at"] = $created_at;
+                
 
                 // Redirect user to the home page
                 header("location: index.php");
